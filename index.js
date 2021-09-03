ea9b98e6f3ecc862918b445dfce9e29c377819da
Giammarco R. Casanova <grcasanova@users.noreply.github.com>
2021-09-03 13:10:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const MUSTACHE_MAIN_DIR = './main.mustache';
 */
 let DATA = {
   name: 'Giammarco',
-  location: 'Brussels',
+  location: 'lost...',
   date: new Date().toLocaleDateString('en-GB', {
     weekday: 'long',
     month: 'long',
