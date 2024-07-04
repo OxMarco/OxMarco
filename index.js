dce9998920a12a39d582c02ff2aaac1d58dc1c71
OxMarco <10283625+OxMarco@users.noreply.github.com>
2024-07-04 10:31:11 +0200

    🇲🇹

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const MUSTACHE_MAIN_DIR = './main.mustache';
 */
 let DATA = {
   name: 'Marco',
-  location: 'Berlin',
+  location: 'Valletta',
   date: new Date().toLocaleDateString('en-GB', {
     weekday: 'long',
     month: 'long',
